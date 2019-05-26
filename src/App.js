6794de6cb67204b5d42a667f036a943e97fc91cd
Raymond Fwu <rayfwu@gmail.com>
2019-05-26 02:03:37 -0500

    added some react route functionality though I might remove it later

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReferenceRequest from './components/ReferenceRequest';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Result from './components/Result';
-import TextField from '@material-ui/core/TextField';
 // import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Welcome to the Credit Clerk!
-        </p>
-        {/* <ReferenceRequest /> */}
-        <Result token="This is a token" custnum="This is a customer number"/>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <header className="App-header">
+            <p>
+              Welcome to the Credit Clerk!
+            </p>
+            <Route exact path="/" component={ReferenceRequest} />
+            <Route path="/result" component={Result} />
+            {/* <ReferenceRequest /> */}
+            {/* <Result token="This is a token" custnum="This is a customer number"/> */}
+          </header>
+        </Router>
+      </div>
+    );
+    }
 }
 
 export default App;
